63350e4c45e0b5b8fadfc1844b3157045a02ef6b
Dayron124 <darrenkeithwhite123@gmail.com>
2021-09-24 09:53:54 +0100

    Added Text-To-Speech
    Added Text-To-Speech and cleaned up code

--- pyda.py
+++ pyda.py
@@ -1,17 +1,19 @@
 # app_id LRE6ER-4ELW4AL7R2
 # app_name pyda.py
 
+import pyttsx3
 from tkinter.constants import FALSE
 import wolframalpha
 import wikipedia
 import PySimpleGUI as sg
 
+# wolfram accessing api
 client = wolframalpha.Client("LRE6ER-4ELW4AL7R2")
 
 
 sg.theme('Dark2')   # Add a touch of color
 # All the stuff inside your window.
-layout = [[sg.Text('Enter a question'), sg.InputText()], [
+layout = [[sg.Text('Enter a question.' '\nBe as specific as possible'), sg.InputText()], [
     sg.Button('Ok'), sg.Button('Cancel')]]
 window = sg.Window('Python Virtual Assistant', layout)
 
@@ -20,12 +22,17 @@ while True:
     event, values = window.read()
     if event in (None, 'Cancel'):  # if user closes window or clicks cancel
         break
-    try:
-        wiki_res = wikipedia.summary(values[0], sentences=2)
-    except:
-        res = client.query(values[0])
-        wolfram_res = next(res.results).text
+    res = client.query(values[0])
+    wolfram_res = next(res.results).text
+    wiki_res = wikipedia.summary(values[0], sentences=2)
+    engine = pyttsx3.init()
+    sg.Popup("Wolfram Results: \n"+wolfram_res,
+             "Wikipedia Results: \n"+wiki_res)
+    engine.say(wolfram_res)
+    engine.say(wiki_res)
+    engine.runAndWait()
+
     print(values[0])
-    sg.Popup(wiki_res)
+
 
 window.close()
