1b51700e156f0492c9f7afc7e90af8d4fa4d73ce
Dayron124 <darrenkeithwhite123@gmail.com>
2021-09-24 15:45:29 +0100

    Update Text-To-Speech and Error reports

--- pyda.py
+++ pyda.py
@@ -13,23 +13,37 @@ client = wolframalpha.Client("LRE6ER-4ELW4AL7R2")
 
 sg.theme('Dark2')   # Add a touch of color
 # All the stuff inside your window.
-layout = [[sg.Text('Enter a question.' '\nBe as specific as possible'), sg.InputText()], [
+layout = [[sg.Text('Enter a question.'), [sg.Input()]], [
     sg.Button('Ok'), sg.Button('Cancel')]]
+[sg.Button('Submit, visible=False, bind_return_key=True')]
 window = sg.Window('Python Virtual Assistant', layout)
 
 # Event Loop to process "events" and get the "values" of the inputs
+engine = pyttsx3.init()
+engine.setProperty('volume', 0.5)
 while True:
     event, values = window.read()
+
     if event in (None, 'Cancel'):  # if user closes window or clicks cancel
         break
-    res = client.query(values[0])
-    wolfram_res = next(res.results).text
-    wiki_res = wikipedia.summary(values[0], sentences=2)
-    engine = pyttsx3.init()
-    sg.Popup("Wolfram Results: \n"+wolfram_res,
-             "Wikipedia Results: \n"+wiki_res)
-    engine.say(wolfram_res)
-    engine.say(wiki_res)
+    try:
+        wiki_res = wikipedia.summary(values[0], sentences=2)
+        wolfram_res = next(client.query(values[0]).results).text
+        engine.say(wolfram_res)
+        sg.PopupNonBlocking("Wolfram Results: \n"+wolfram_res,
+                            "Wikipedia Results: \n"+wiki_res)
+    except wikipedia.exceptions.DisambiguationError:
+        wolfram_res = next(client.query(values[0]).results).text
+        engine.say(wolfram_res)
+        sg.PopupNonBlocking(wolfram_res)
+    except wikipedia.exceptions.PageError:
+        wolfram_res = next(client.query(values[0]).results).text
+        engine.say(wolfram_res)
+        sg.PopupNonBlocking(wolfram_res)
+    except:
+        wiki_res = wikipedia.summary(values[0], sentences=2)
+        engine.say(wiki_res)
+        sg.PopupNonBlocking(wiki_res)
     engine.runAndWait()
 
     print(values[0])
